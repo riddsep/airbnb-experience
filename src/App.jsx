95e023502e9b8e38fa7b959b61d44ca60ba6acd1
riddsep <ridoseptiawan00@gmail.com>
2024-09-12 17:10:33 +0700

    feat: add props

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,34 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <div className="card-container">
+        <Card
+          img="../src/assets/card-1.png"
+          status="SOLD OUT"
+          rating="5.0"
+          count="6"
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price="136"
+        />
+        <Card
+          img="../src/assets/card-2.png"
+          status="ONLINE"
+          rating="5.0"
+          count="30"
+          country="SGP"
+          title="Learn wedding photography"
+          price="125"
+        />
+        <Card
+          img="../src/assets/card-3.png"
+          rating="4.8"
+          count="2"
+          country="IDN"
+          title="Group Mountain Biking"
+          price="50"
+        />
+      </div>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,67 +1,29 @@
+/* eslint-disable react/prop-types */
 import "../components/css-modules/Card.css";
-import CardImage1 from "../assets/card-1.png";
 import Star from "../assets/star.svg";
 
-function Card() {
+function Card(props) {
+  const { img, status, rating, count, country, title, price } = props;
   return (
-    <div className="card-container">
-      <div className="card">
-        <div className="card-image">
-          <p>SOLD OUT</p>
-          <img src={CardImage1} alt="" />
-        </div>
-        <div className="card-info">
-          <div className="card-rating">
-            <img src={Star} alt="" />
-            5.0
-            <span> (6) &#x2022; USA</span>
-          </div>
-          <div className="card-title">
-            <p>Life Lessons with Katie Zaferes</p>
-            <p className="card-price">
-              From $136 <span>/ person</span>
-            </p>
-          </div>
-        </div>
+    <div className="card">
+      <div className="card-image">
+        {status && <p>{status}</p>}
+        <img src={img} alt="" />
       </div>
-
-      <div className="card">
-        <div className="card-image">
-          <p>SOLD OUT</p>
-          <img src={CardImage1} alt="" />
+      <div className="card-info">
+        <div className="card-rating">
+          <img src={Star} alt="" />
+          {rating}
+          <span>
+            {" "}
+            ({count}) &#x2022; {country}
+          </span>
         </div>
-        <div className="card-info">
-          <div className="card-rating">
-            <img src={Star} alt="" />
-            5.0
-            <span> (6) &#x2022; USA</span>
-          </div>
-          <div className="card-title">
-            <p>Life Lessons with Katie Zaferes</p>
-            <p className="card-price">
-              From $136 <span>/ person</span>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="card-image">
-          <p>SOLD OUT</p>
-          <img src={CardImage1} alt="" />
-        </div>
-        <div className="card-info">
-          <div className="card-rating">
-            <img src={Star} alt="" />
-            5.0
-            <span> (6) &#x2022; USA</span>
-          </div>
-          <div className="card-title">
-            <p>Life Lessons with Katie Zaferes</p>
-            <p className="card-price">
-              From $136 <span>/ person</span>
-            </p>
-          </div>
+        <div className="card-title">
+          <p>{title}</p>
+          <p className="card-price">
+            From ${price} <span>/ person</span>
+          </p>
         </div>
       </div>
     </div>
